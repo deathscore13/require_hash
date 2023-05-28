8c8def77cda5d152804f3e07ba03b86502ae7f12
DeathScore13 <deathscore13@gmail.com>
2023-05-28 17:17:12 +0300

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # require_hash
-Безопасное подключение зависимостей для PHP 8.0.0+
+### Безопасное подключение зависимостей для PHP 8.0.0+<br><br>
+
+**Библиотеку можно подключать в одном проекте несколько раз**<br><br>
+Содержит процедуры `require_hash()` (аналог `require_once()`) и `include_hash()` (аналог `include_once()`)
+
+<br><br>
+## Пример использования
+**test.php**:
+```php
+// допустим, подключаемому файлу он тоже нужен
+require('require_hash.php');
+
+class BaseClass
+{
+}
+
+// возвращаемое значение из файла
+return 123;
+```
+**test2.php**:
+```php
+// допустим, подключаемому файлу он тоже нужен
+require('require_hash.php');
+
+class BaseClass
+{
+}
+
+// возвращаемое значение из файла
+return 123;
+```
+**main.php**:
+```php
+// подключение require_hash
+require('require_hash.php');
+
+// повторное подключение require_hash.php не вызовет ошибку и выведет 123
+echo(require_hash('test.php').PHP_EOL);
+
+// выведет 1 (true), так как файл с таким содержанием уже подключен
+echo(require_hash('test2.php').PHP_EOL);
+
+// оператор @ подавит предупреждение и выведет 0 (false), так как файла не существует
+echo((@include_hash('test3.php') ? 1 : 0).PHP_EOL);
+```
---
+++ require_hash.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * require_hash
+ * 
+ * Безопасное подключение зависимостей для PHP 8.0.0+
+ * https://github.com/deathscore13/require_hash
+ */
+
+if (defined('DEATHSCORE13_REQUIRE_HASH'))
+    return;
+
+const DEATHSCORE13_REQUIRE_HASH = true;
+
+/**
+ * Аналог require_once, но не подключает файл с одинаковым содержанием
+ * 
+ * @param string $file      Путь к файлу
+ * 
+ * @return mixed            Возвращаемое значение из файла или true если уже подключен
+ */
+function require_hash(string $file): mixed
+{
+    static $data = [];
+    
+    if (!is_file($file))
+        throw new Exception('Failed opening required \''.$file.'\' (include_path=\''.get_include_path().'\')');
+    
+    $hash = hash_file('md5', $file);
+    
+    if (in_array($hash, $data))
+        return true;
+    
+    $data[] = $hash; 
+    return require($file);
+}
+
+/**
+ * Аналог include_once, но не подключает файл с одинаковым содержанием
+ * 
+ * @param string $file      Путь к файлу
+ * 
+ * @return mixed            Возвращаемое значение из файла, true если уже подключен или false если файл не найден
+ */
+function include_hash(string $file): mixed
+{
+    static $data = [];
+    
+    if (!is_file($file))
+    {
+        trigger_error('Failed opening required \''.$file.'\' (include_path=\''.get_include_path().'\')', E_USER_WARNING);
+        return false;
+    }
+    
+    $hash = hash_file('md5', $file);
+    
+    if (in_array($hash, $data))
+        return true;
+    
+    $data[] = $hash; 
+    return require($file);
+}
